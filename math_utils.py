53216ba73e72ef3a560c4b09a65fd1859be5d07d
YaelIzhak <yaelizhak2001@gmail.com>
2025-10-30 12:00:38 +0200

    Update math_utils.py
    5

--- math_utils.py
+++ math_utils.py
@@ -1,8 +1,8 @@
 def find_max_number(T,C,B):
     if(T > C) and (T > B):
-     return 'T'
+     return T
     elif(C > B):
-      return 'C'
+     return C
     else:
-       return 'B'
+     return B
 
